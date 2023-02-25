e4ba954108485e99139bede60b504cdbed31b78f
infernious dude <inferniousdude@gmail.com>
2023-02-25 17:51:01 -0600

    added events sign-up view to /eventannounce

--- cogs/miscellaneous.py
+++ cogs/miscellaneous.py
@@ -8,6 +8,30 @@ from cogs.moderation import convert
 blue = 0x00ffff
 
 
+class EventAnnouncement(discord.ui.View):
+
+    def __init__(self, bot):
+        super().__init__()
+        self.bot = bot
+
+    @discord.ui.button(label='Click here to be notified for future events!', style=discord.ButtonStyle.gray)
+    async def callback(self, interaction, button):
+
+        """
+        Give the "Lounge: Events" role to the interaction user.
+        """
+
+        role = discord.utils.get(interaction.guild.roles, name="Lounge: Events")
+        member = interaction.guild.get_member(interaction.user.id)
+
+        if role in member.roles:
+            await member.remove_roles(role)
+            await interaction.response.send_message(f"`{role.name}` role removed!", ephemeral=True)
+        else:
+            await member.add_roles(role)
+            await interaction.response.send_message(f"`{role.name}` role added!", ephemeral=True)
+
+
 class EventConfirm(discord.ui.View):
 
     def __init__(self, bot):
@@ -24,7 +48,7 @@ class EventConfirm(discord.ui.View):
         event_role = discord.utils.get(interaction.guild.roles, name="Lounge: Events")
         event_channel = discord.utils.get(interaction.guild.channels, name="events")
 
-        await event_channel.send(f"{event_role.mention}")
+        await event_channel.send(f"{event_role.mention}", view=EventAnnouncement(self.bot))
 
         button.style = discord.ButtonStyle.grey
         button.label = "Event announced!"
