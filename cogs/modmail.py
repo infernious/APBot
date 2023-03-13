58692cf032a2175226e5ea3c015d50615e03ba96
infernious dude <inferniousdude@gmail.com>
2023-03-12 20:52:35 -0500

    added thread implementation

--- cogs/modmail.py
+++ cogs/modmail.py
@@ -6,6 +6,7 @@ import datetime
 
 modmail_id = 1060459641244500038
 blue = 0x00ffff
+orange = 0xffa07a
 
 
 class Modmail(commands.Cog):
@@ -30,7 +31,7 @@ class Modmail(commands.Cog):
         """
         Sends messages sent to the bot's DMs to the modmail channel.
             - Checks if the message is not from the bot and if the message is not from a server.
-            - Creates a modmail embed to send.
+            - Creates a modmail embed to send in thread.
         """
 
         if message.author != self.bot.user:
@@ -40,58 +41,72 @@ class Modmail(commands.Cog):
 
             bot_config = await self.bot.read_user_config(self.bot.application_id)
             if message.author.id in bot_config["modmail_users"]:
-                await message.author.send(f"You have already contacted the mods recently. "
-                                          f"You are able to send another message "
-                                          f"{discord.utils.format_dt(self.delay.next_iteration, style='R')}.")
+                await message.author.send(
+                    f"You have already contacted the mods recently. You are able to send another message "
+                    f"{discord.utils.format_dt(self.delay.next_iteration, style='R')}.")
 
-            else:
-
-                modmail_embed = discord.Embed(title="", color=blue)
-                modmail_embed.add_field(
-                    name=f"Message from {message.author.display_name}#{message.author.discriminator}"
-                         f" (ID: {message.author.id})",
-                    value=f'{message.content} \u200b\n - {message.author.mention}', inline=False)
-                modmail_embed.timestamp = datetime.datetime.now()
-                guild = self.bot.get_guild(self.bot.guild_id)
-                modmail = discord.utils.get(guild.channels, name="modmail")
-
-                attachments_boolean = False
-                attachments_count = 1
-                attachment_embeds = []
-                # Check if the message has any attachments or image URLs
-                if message.attachments:
-                    # If it does, add the URLs to the string
-                    if len(message.attachments) > 1:
-
-                        attachments_boolean = True
-
-                        for attachment in message.attachments:
-                            attachment_embed = discord.Embed(title="", color=blue)
-                            attachment_embed.set_image(url=attachment.proxy_url)
-                            attachment_embed.set_footer(
-                                text=f"{attachments_count + 1}/{len(message.attachments) + 1} attachments | "
-                                     f"{message.author.display_name}#{message.author.discriminator}")
-                            attachment_embeds.append(attachment_embed)
-                            attachments_count += 1
-
-                    else:
-                        modmail_embed.set_footer(text="1/1 attachment")
-                        modmail_embed.set_image(url=message.attachments[0].proxy_url)
-
-                await modmail.send(embed=modmail_embed)
-
-                if attachments_boolean:
-                    for attachment_embed in attachment_embeds:
-                        await modmail.send(embed=attachment_embed)
+            elif isinstance(message.channel, discord.DMChannel) and message.author != self.bot.user:
 
                 await message.author.send("Your message has been sent to the mod inbox. We will contact you if needed.")
+
+                modmail_embed = discord.Embed(title="", color=blue)
+                modmail_embed.add_field(name=f"", value=f'{message.content}\n - {message.author.mention}',
+                                        inline=False)
+                modmail_embed.timestamp = datetime.datetime.now()
+
+                if message.attachments:
+                    modmail_embed.set_image(url=message.attachments[0].proxy_url)
+
+                guild = self.bot.get_guild(self.bot.guild_id)
+                user_config = await self.bot.read_user_config(message.author.id)
+
+                try:
+                    thread = await guild.fetch_channel(user_config["modmail_id"])
+                    await thread.send(embed=modmail_embed)
+                    await thread.starter_message.clear_reactions()
+                except (KeyError, discord.errors.NotFound):
+                    modmail = discord.utils.get(guild.channels, name="modmail")
+                    create_thread = await modmail.create_thread(
+                        name=f"{message.author.name}#{message.author.discriminator}: {message.author.id}",
+                        embed=modmail_embed)
+                    thread = create_thread.thread
+
+                    user_config["modmail_id"] = thread.id
+                    await self.bot.update_user_config(message.author.id, user_config)
+
+                if len(message.attachments) > 1:
+
+                    attachments_count = 1
+
+                    for attachment in message.attachments:
+
+                        attachment_embed = discord.Embed(title="", color=blue)
+
+                        try:
+                            attachment_embed.set_image(url=message.attachments[attachments_count].proxy_url)
+                            attachment_embed.set_footer(text=f"{attachments_count + 1}/{len(message.attachments)} attachments")
+                        except IndexError:
+                            break
+
+                        attachments_count += 1
+                        await thread.send(embed=attachment_embed)
+
                 bot_config = await self.bot.read_user_config(self.bot.application_id)
                 bot_config["modmail_users"].append(message.author.id)
                 await self.bot.update_user_config(self.bot.application_id, bot_config)
 
+    @commands.Cog.listener()
+    async def on_reaction_add(self, reaction, user):
+        try:
+            if reaction.message.channel.parent.name == "modmail":
+                await reaction.message.channel.edit(archived=True)
+        except AttributeError:
+            return
+
     @app_commands.checks.has_permissions(moderate_members=True)
     @app_commands.command(name='send', description='Send messages to other members through modmail.')
-    async def send(self, interaction: discord.Interaction, member: discord.User, *, message: str, attachment: discord.Attachment = None):
+    async def send(self, interaction: discord.Interaction, member: discord.User, *, message: str,
+                   attachment: discord.Attachment = None):
 
         """
         Send messages to server members (cannot send message to users not in a server with the bot).
@@ -99,14 +114,14 @@ class Modmail(commands.Cog):
             - Checks if member is in the cooldown list and removes them if so.
         """
 
-        send_embed = discord.Embed(title="", color=blue)
+        send_embed = discord.Embed(title="", color=orange)
         send_embed.add_field(name="Message from the mods!", value=f"{message} \u200b")
         send_embed.timestamp = datetime.datetime.now()
         if attachment:
             send_embed.set_image(url=attachment.proxy_url)
         await member.send(embed=send_embed)
 
-        response_embed = discord.Embed(title="", color=blue)
+        response_embed = discord.Embed(title="", color=orange)
         response_embed.add_field(name=f"Message sent to {member.name}#{member.discriminator}!",
                                  value=f"```{message}```")
         response_embed.timestamp = datetime.datetime.now()
