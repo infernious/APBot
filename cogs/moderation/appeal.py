064d78a6a59418347bf5ea0581eb805d417c778f
infernious dude <inferniousdude@gmail.com>
2023-03-15 17:41:19 -0500

    automation of ban appeals

---
+++ cogs/moderation/appeal.py
@@ -0,0 +1,65 @@
+import discord
+from discord.ext import tasks, commands
+
+import datetime
+
+yellow = 0xffff00
+orange = 0xffa500
+light_orange = 0xffa07a
+dark_orange = 0xff5733
+red = 0xff0000
+green = 0x00ff00
+
+
+class BanAppeal(commands.Cog):
+
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+        self.appeal_loop.start()
+
+    @tasks.loop(days=1)
+    async def appeal_loop(self):
+
+        """
+        Checks every day for ban appeals.
+        """
+
+        guild = self.bot.get_guild(self.bot.guild_id)
+        channel = discord.utils.get(guild.channels, name="important-updates")
+        cursor = self.bot.user_config.find({"check_appeal_date": {"$lte": datetime.datetime.now()}})
+        documents = await cursor.to_list(length=100)
+
+        for document in documents:
+
+            appeal_message = await channel.fetch_message(document["appeal_message_id"])
+
+            maintain_ban_reaction = discord.utils.get(appeal_message.reactions, emoji="🔴")
+            unban_reaction = discord.utils.get(appeal_message.reactions, emoji="🟢")
+
+            thread = discord.utils.find(lambda c: f"({document['user_id']})" in c.name, appeal_message.channel.threads)
+            decision_embed = discord.Embed(title='')
+
+            if unban_reaction.count > maintain_ban_reaction.count:
+                try:
+                    decision_embed.colour = green
+                    decision_embed.add_field(name="", value="Member will be unbanned!")
+                    await thread.send(embed=decision_embed)
+                    await guild.unban(discord.Object(id=document["user_id"]))
+                except discord.errors.NotFound:
+                    decision_embed.colour = orange
+                    decision_embed.add_field(name="", value="Member is already unbanned!")
+            else:
+                decision_embed.colour = red
+                decision_embed.add_field(name="", value="Member will remain banned!")
+
+            await thread.edit(archived=True)
+            await self.bot.user_config.update_one({"_id": document["_id"]}, {"$unset": {"check_appeal_date": ""}})
+            await self.bot.user_config.update_one({"_id": document["_id"]}, {"$unset": {"appeal_message_id": ""}})
+
+    @appeal_loop.before_loop
+    async def appeal_before_loop(self):
+        await self.bot.wait_until_ready()
+
+
+async def setup(bot: commands.Bot) -> None:
+    await bot.add_cog(BanAppeal(bot), guilds=[discord.Object(id=bot.guild_id)])
