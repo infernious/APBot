1fe2572dc0b3222423163d52bcff3b02867eb803
infernious dude <inferniousdude@gmail.com>
2023-07-08 12:47:01 -0500

    delete message context menu

--- cogs/moderation/commands.py
+++ cogs/moderation/commands.py
@@ -74,7 +74,7 @@ class BanAppeal(discord.ui.Modal, title="Ban Appeal"):
 
     async def on_submit(self, interaction: discord.Interaction):
 
-        await interaction.response.send_message(f"Your appeal has been sent!")
+        await interaction.response.send_message(f"Your appeal has been sent!", ephemeral=True)
 
         guild = self.bot.get_guild(self.bot.guild_id)
         appeal_channel = discord.utils.get(guild.channels, name="important-updates")
@@ -115,7 +115,7 @@ class BanAppealButton(discord.ui.View):
         super().__init__(timeout=None)
         self.bot = bot
 
-    @discord.ui.button(label='Appeal Ban', style=discord.ButtonStyle.grey, custom_id="appeal")
+    @discord.ui.button(label='Appeal Ban', style=discord.ButtonStyle.blurple, custom_id="appeal")
     async def callback(self, interaction, button):
         """
         Confirm to appeal ban to bring up the modal.
@@ -135,6 +135,14 @@ class ModerationCommands(commands.Cog):
 
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
+        self.ctx_menu = app_commands.ContextMenu(
+            name="Delete Message",
+            callback=self.delete
+        )
+        self.bot.tree.add_command(self.ctx_menu)
+
+    async def cog_unload(self) -> None:
+        self.bot.tree.remove_command(self.ctx_menu.name, type=self.ctx_menu.type)
 
     @app_commands.checks.has_permissions(moderate_members=True)
     @app_commands.command(name='warn', description="Warn members of rule-breaking behavior.")
@@ -476,6 +484,8 @@ class ModerationCommands(commands.Cog):
         ban_message.add_field(name='You have been banned!',
                               value=f'Reason: {reason}',
                               inline=False)
+        ban_message.add_field(name='Appeal',
+                              value='If you wish to appeal your ban, you may do so by joining the following server: https://discord.gg/RHx7deYQ3q')
         ban_message.timestamp = datetime.datetime.now()
 
         ban_log = discord.Embed(title=f"{member.name}#{member.discriminator} has been banned.", color=red)
@@ -495,7 +505,7 @@ class ModerationCommands(commands.Cog):
             ban_response.set_image(url=attachment.proxy_url)
 
         try:
-            await member.send(embed=ban_message, view=BanAppealButton(self.bot))
+            await member.send(embed=ban_message)
         except Exception:
             ban_log.set_footer(text=f"Could not DM ban message.")
         # Ban AFTER message is sent.
@@ -571,136 +581,52 @@ class ModerationCommands(commands.Cog):
         member_config["infractions"].append(warning)
         await self.bot.update_user_config(member_id, member_config)
 
-    @app_commands.checks.has_permissions(moderate_members=True)
-    @app_commands.command(name='warnings', description="Show infraction history of a member.")
-    async def warnings(self, interaction: discord.Interaction, user: discord.User):
+    @app_commands.checks.has_permissions(manage_messages=True)
+    async def delete(self, interaction:discord.Interaction, message: discord.Message):
 
         """
-        Retrieve full infraction history of a member.
-            - Reads each infraction in infraction history list.
-            - Sends an embed for each infraction.
+        Deletes a message using the bot.
         """
 
-        await interaction.response.send_message(f"Fetching {user.mention}'s warnings...")
-        member_config = await self.bot.read_user_config(user.id)
-        infractions  = member_config["infractions"]
-        count = 1
-        for infraction in infractions:
-            match infraction["type"]:
-                # /warn
-                case "warn":
-                    infraction_embed = discord.Embed(title="", color=yellow)
-                    infraction_embed.add_field(name="Warn", value=f"Reason: {infraction['reason']}\n"
-                                                                  f"Responsible Mod: {infraction['moderator']}")
-                # /wm (NOT /mute)
-                case "mute":
-                    infraction_embed = discord.Embed(title="", color=orange)
-                    infraction_embed.add_field(name="Mute", value=f"Reason: {infraction['reason']}\n"
-                                                                  f"Duration: {infraction['duration']}\n"
-                                                                  f"Responsible Mod: {infraction['moderator']}")
-                    try:
-                        infraction_embed.add_field(name="Unmute", value=f"Reason: {infraction['unmute reason']}\n"
-                                                                        f"Responsible Mod: {infraction['moderator']}")
-                        infraction_embed.color = green
-                    except KeyError:
-                        pass
-                case "kick":
-                    infraction_embed = discord.Embed(title="", color=dark_orange)
-                    infraction_embed.add_field(name="Warn", value=f"Reason: {infraction['reason']}\n"
-                                                                  f"Responsible Mod: {infraction['moderator']}")
-                # /ban
-                case "ban":
-                    infraction_embed = discord.Embed(title="", color=red)
-                    infraction_embed.add_field(name="Ban", value=f"Reason: {infraction['reason']}\n"
-                                                                 f"Responsible Mod: {infraction['moderator']}")
-                # /forceban
-                case "force-ban":
-                    infraction_embed = discord.Embed(title="", color=red)
-                    infraction_embed.add_field(name="Force-Ban", value=f"Reason: {infraction['reason']}\n"
-                                                                       f"Responsible Mod: {infraction['moderator']}")
+        await interaction.response.defer(ephemeral=True)
+        await message.delete()
 
-            infraction_embed.set_footer(text=f"{count}/{len(infractions)} infractions")
-            count += 1
-            infraction_embed.timestamp = infraction["date"]
-            try:
-                infraction_embed.set_image(url=infraction['attachment'])
-            except KeyError:
-                pass
-            await interaction.channel.send(embed=infraction_embed)
+        message_log = discord.Embed(title=f"", color=yellow)
+        message_log.add_field(name=f"Responsible Moderator: ",
+                              value=f"{interaction.user.name}#{interaction.user.discriminator}",
+                              inline=False)
+        message_log.add_field(name="Message Content: ", value=f"```{message.content}```", inline=False)
+        message_log.add_field(name="Message Channel: ", value=f"{message.channel.mention}", inline=False)
+        message_log.set_author(name=f"{message.author.name}#{message.author.discriminator}'s message was deleted.",
+                               url=f"{message.author.avatar.url}")
+        message_log.timestamp = datetime.datetime.now()
 
-        await interaction.followup.send(f"Complete, all infractions shown! {user.mention} has "
-                                        f"{member_config['infraction_points']} infraction points.")
+        if message.attachments:
+            message_log.set_image(url=message.attachments[0].proxy_url)
 
-    @app_commands.checks.has_permissions(moderate_members=True)
-    @app_commands.command(name='editip', description="Edit a member's infraction points.")
-    async def editip(self, interaction: discord.Interaction, member: discord.Member, change: int):
+        guild = self.bot.get_guild(self.bot.guild_id)
+        logs = discord.utils.get(guild.channels, name="logs")
 
-        """
-        Edit a member's infraction points.
-            - Retrieves and adds specified amount to infraction points.
-            - Checks if infraction points exceed 30.
-        """
+        await logs.send(embed=message_log)
 
-        member_config = await self.bot.read_user_config(member.id)
-        member_config["infraction_points"] += change
-        await self.bot.update_user_config(member.id, member_config)
+        if len(message.attachments) > 1:
 
-        await interaction.response.send_message(f"Adding {change} to {member.mention}'s infraction points, "
-                                                f"they now have {member_config['infraction_points']} total.\n")
+            attachments_count = 1
 
-        if member_config["infraction_points"] >= 30:
+            for attachment in message.attachments:
 
-            guild = self.bot.get_guild(self.bot.guild_id)
-            update = discord.utils.get(guild.channels, name="important-updates")
-            chat_mod = discord.utils.get(guild.roles, name='Chat Moderator')
+                attachment_embed = discord.Embed(title="", color=blue)
 
-            ban_warn_embed = discord.Embed(title='', color=red)
-            ban_warn_embed.add_field(name='Member has reached 30 infraction points!',
-                                     value=f'{member.mention} has {member_config["infraction_points"]} infraction points'
-                                           f' and should be reviewed for a ban.')
+                try:
+                    attachment_embed.set_image(url=message.attachments[attachments_count].proxy_url)
+                    attachment_embed.set_footer(text=f"{attachments_count + 1}/{len(message.attachments)} attachments")
+                except IndexError:
+                    break
 
-            await update.send(f"{chat_mod.mention}", embed=ban_warn_embed)
+                attachments_count += 1
+                await logs.send(embed=attachment_embed)
 
-    @app_commands.checks.has_permissions(moderate_members=True)
-    @app_commands.command(name='userip', description="View a member's infraction points.")
-    async def userip(self, interaction: discord.Interaction, member: discord.Member):
-
-        """
-        Displays current infraction points of a user.
-        """
-
-        member_config = await self.bot.read_user_config(member.id)
-        await interaction.response.send_message(f"{member.mention} has {member_config['infraction_points']} infraction point(s).")
-
-    @app_commands.command(name='infpoints', description="View how many infraction points you have.")
-    async def infpoints(self, interaction: discord.Interaction):
-
-        """
-        Returns command's user infraction points.
-        """
-
-        member_config = await self.bot.read_user_config(interaction.user.id)
-        await interaction.response.send_message(
-            f"You have **{member_config['infraction_points']} infraction point(s).**", ephemeral=True)
-
-    @app_commands.checks.has_role("Bot Staff")
-    @app_commands.command()
-    async def unload(self, interaction: discord.Interaction, cog: str):
-        await self.bot.unload_extension(f"cogs.{cog}")
-        await interaction.response.send_message(f'`cogs.{cog} unloaded`')
-
-    @app_commands.checks.has_role("Bot Staff")
-    @app_commands.command()
-    async def load(self, interaction: discord.Interaction, cog: str):
-        await self.bot.load_extension(f"cogs.{cog}")
-        await interaction.response.send_message(f'`cogs.{cog} loaded`')
-
-    @app_commands.checks.has_role("Bot Staff")
-    @app_commands.command()
-    async def reload(self, interaction: discord.Interaction, cog: str):
-        await self.bot.unload_extension(f"cogs.{cog}")
-        await self.bot.load_extension(f"cogs.{cog}")
-        await interaction.response.send_message(f'`cogs.{cog} reloaded`')
+        await interaction.followup.send("`Message successfully deleted!`", ephemeral=True)
 
 
 async def setup(bot: commands.Bot) -> None:
