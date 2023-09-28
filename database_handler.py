6766bfa9f766d0b4f37eef5b640aaf0e4c3efd3e
ThePriveatePanda <parth@privatepanda.co>
2023-09-29 00:51:31 +0530

    v3 cogs.moderation.appeal

--- database_handler.py
+++ database_handler.py
@@ -1,99 +1,20 @@
-import motor.motor_asyncio as motor
-from config_handler import Config
+import json
+import os
 from datetime import datetime
+from typing import List, Optional, Union
+
+import motor.motor_asyncio as motor
+
+from config_handler import Config
+
 
 class Database:
     def __init__(self, conf: Config):
-        self.database_client = motor.AsyncIOMotorClient(conf.get("mongo_connect_url"))
+        self.bot_user_id: int
+        self.database_client = motor.AsyncIOMotorClient(os.environ.get("DATABASE_PASSWORD"))
         self.database = self.database_client["ap-students"]
-
+        self.user_config = self.database["user_config"]
         self.conf = conf
-
-    async def set_user_study_expiry(self, user_id, study_expires_at):
-        # add a user to the database with study expires_at epoch timestamp
-        ...
-
-    async def get_user_study_expiry(self, user_id) -> int:
-        # return the epoch timestamp when the user's study role should be removed at
-        ...
-
-    async def get_all_study_students(self) -> dict[int, int]:
-        # return a dict of users with study role like so:
-        # {
-        #     "USER_ID: int": "STUDY_EXPIRES_AT: int",
-        #     "USER_ID: int": "STUDY_EXPIRES_AT: int",
-        #     "USER_ID: int": "STUDY_EXPIRES_AT: int"
-        # }
-        ...
-
-    async def set_reminder(self, user_id: int, expires_at: int, message: str):
-        # Add a reminder
-        ...
-
-    async def remove_bonk(self, user_id: int, expires_at: int, message: str):
-        # Remove reminder for user_id which has given params
-        ...
-
-    async def remove_all_bonks(self, user_id: int):
-        # remove all bonks for user with user_id
-        ...
-
-    async def get_all_user_reminders(self, user_id: int):
-        # return a list of reminders like soo:
-        # [
-        #     [
-        #         "EXPIRES_AT: int",
-        #         "MESSAGE: str"
-        #     ],
-        #     [
-        #         "EXPIRES_AT: int",
-        #         "MESSAGE: str"
-        #     ]
-        # ]
-        ...
-
-    async def get_all_reminders(self):
-        # Return all reminders known to the bot in this format:
-        # [
-        #         [
-        #             "USER_ID: INT",
-        #             "EXPIRES_AT: int",
-        #             "MESSAGE: str"
-        #         ],
-        #         [
-        #             "USER_ID: INT",
-        #             "EXPIRES_AT: int",
-        #             "MESSAGE: str"
-        #         ]
-        # ]
-        ...
-
-    async def get_decay_date(self) -> datetime:
-        # return datetime object
-        ...
-    
-    async def remove_one_inf(self) -> datetime:
-        # return next decay date datetime obj. Return `false` if function failed
-        # I believe it is something like this:
-        # await self.bot.user_config.update_many({'infraction_points': {'$gt': 0}}, {'$inc': {'infraction_points': -1}})
-        #
-        # Also, set decay_date to +7 days.
-        ...
-
-    async def get_appeals(self) -> list[dict[str, str]]:
-        # Return all appeal messages that should be processed, like so:
-        # {
-        #     "USER_ID: int": "MESSAGE_ID: int",
-        #     "USER_ID: int": "MESSAGE_ID: int",
-        #     "USER_ID: int": "MESSAGE_ID: int"
-        # }
-        ...
-    
-    async def remove_pending_appeal(self, user_id):
-        ...
-
-    async def get_user_infractions(self, user_id):
-        ...
 
     async def read_user_config(self, user_id: int):
         config_from_db = await self.database["user_config"].find_one({"user_id": user_id})
@@ -113,3 +34,151 @@ class Database:
 
         _id = old_config["_id"]
         await self.database["user_config"].replace_one({"_id": _id}, new_config)
+
+    async def set_user_study_end(self, user_id: int, study_expires_at: int):
+        """Set the time at which study will expire for a particular user"""
+        user_config = await self.read_user_config(user_id)
+        user_config["study_expires_at"] = study_expires_at
+        await self.update_user_config(user_id, user_config)
+
+    async def get_user_study_end(self, user_id: int) -> Optional[int]:
+        """Get the time at which study expires for a particular user"""
+        user_config = await self.read_user_config(user_id)
+        if "study_expires_at" in user_config:
+            return user_config["study_expires_at"]
+        else:
+            return None
+
+    async def get_all_study_students(self) -> dict[int, int]:
+        """Return all users with the study role"""
+        cursor = self.user_config.find({"study_expires_at": {"$exists": True}})
+        all_study_students = {}
+        for document in cursor:
+            all_study_students[document["user_id"]] = document["study_expires_at"]
+        return all_study_students
+
+    async def delete_study_user(self, user_id: int):
+        """Remove a studying user from the database"""
+        ...
+
+    # Reminders
+    async def get_all_user_reminders(self, user_id: int) -> dict[str, dict[str, Union[str, int]]]:
+        """Get all reminders of a particular user"""
+        # return a dict of reminders like so:
+        # {
+        #     "reminder_id": {
+        #         "start": "start_time: INT",
+        #         "end": "end_time: INT",
+        #         "message": "message: STR",
+        #     },
+        #     "reminder_id": {
+        #         "start": "start_time: INT",
+        #         "end": "end_time: INT",
+        #         "message": "message: STR",
+        #     }
+        # }
+        ...
+
+    async def set_reminder(self, reminder_id: str, user_id: int, start_time: int, end_time: int, message: str = "") -> None:
+        """Set a new reminder for a particular user"""
+        ...
+
+    async def remove_reminder(self, user_id: int, reminder_id: str) -> None:
+        """Remove a particular reminder for a particular user"""
+        ...
+
+    async def remove_all_user_reminders(self, user_id: int):
+        """Remove all reminders for a particular user"""
+        user_config = await self.read_user_config(user_id)
+        user_config["reminders"] = []
+        await self.update_user_config(user_id, user_config)
+
+    async def get_all_reminders(self) -> dict[str, dict[str, Union[str, int]]]:
+        """Get all known reminders in the database"""
+        # Return all reminders known to the bot in this format:
+        # {
+        #     "reminder_id": {
+        #         "user_id": "user_id: INT",
+        #         "start": "start_time: INT",
+        #         "end": "end_time: INT",
+        #         "message": "message: STR",
+        #     },
+        #     "reminder_id": {
+        #         "user_id": "user_id: INT",
+        #         "start": "start_time: INT",
+        #         "end": "end_time: INT",
+        #         "message": "message: STR",
+        #     }
+        # }
+        ...
+
+        pipeline = [
+            {"$match": {"reminders": {"$exists": True}}},
+            {"$unwind": "$reminders"},
+            {"$project": {"_id": 0, "user_id": 1, "expires_at": "$reminders.expires_at", "message": "$reminders.message"}},
+        ]
+
+        cursor = self.user_config.aggregate(pipeline)
+        return list(cursor)
+
+    async def get_modmail_banned_users(self) -> List[int]:
+        """Return a list of User IDs which are banned from using Modmail"""
+        ...
+
+    async def get_user_modmail_channel(self, user_id: int) -> Optional[int]:
+        """Return modmail thread ID if exists, or else return None"""
+        ...
+
+    async def set_user_modmail_channel(self, user_id: int, thread_id: int) -> None:
+        """Set the modmail thread ID for a particular user"""
+        ...
+
+    async def ban_modmail_user(self, user_id: int) -> None:
+        """Ban a user from using modmail"""
+        ...
+
+    async def unban_modmail_user(self, user_id: int) -> None:
+        """Unban a user from using modmail"""
+        ...
+
+    async def set_user_appeal_submission(self, user_id: int, submission_time: int, message_id: int) -> None:
+        """Set the last known appeal submission time. If already set, replace."""
+        ...
+
+    async def get_last_appeal(self, user_id: int):
+        """Return last appeal time and decision"""
+        # return a tuple of data: (last_appeal_time, last_appeal_decision)
+        # last_appeal_time: epoch timestamp of last appeal submission. 0 if not exists
+        # last_appeal_decision:
+        #       None: Not Decided Yet
+        #       True: Unbanned
+        #       False: Remains banned
+        ...
+
+    async def get_all_pending_decisions(self):
+        """Get all the pending ban appeals"""
+        # return a list like this:
+        # [
+        #     [
+        #         user_id,
+        #         submission_time
+        #         message_time
+        #     ],
+        #     [
+        #         user_id,
+        #         submission_time
+        #         message_time
+        #     ]
+        # ]
+
+
+    async def get_decay_date(self) -> datetime:
+        # return datetime object
+        config = await self.read_user_config(self.bot_user_id)
+        return config["decay_day"]
+
+    async def remove_one_inf(self) -> None:
+        config = await self.read_user_config(self.bot_user_id)
+
+        await self.user_config.update_many({"infraction_points": {"$gt": 0}}, {"$inc": {"infraction_points": -1}})
+        config["decay_day"] = config["decay_day"] + datetime.timedelta(days=7)
--- main.py
+++ main.py
@@ -1,4 +1,80 @@
-from bot_base import APBot
+import time
+from datetime import datetime
+from typing import List
 
-bot = APBot()
-bot.run()
+from discord import Guild
+from nextcord import Activity, ActivityType, Intents
+
+from bot_base import APBot
+from config_handler import Config
+from database_handler import Database
+
+conf = Config("config.json")
+
+bot: APBot = APBot(
+    command_prefix=conf.get("command_prefix", "ap:"),
+    strip_after_prefix=True,
+    intents=Intents.all(),
+    activity=Activity(type=ActivityType.playing, name="DM me to contact mods!"),
+    default_guild_ids=[conf.get("guild_id")],
+)
+
+cogs: List[str] = [
+    "jishaku",
+    "cogs.moderation.appeal",
+    # "cogs.moderation.commands",
+    # "cogs.moderation.decay",
+    # "cogs.bonk",
+    # "cogs.errorhandler",
+    # "cogs.events",
+    # "cogs.modmail",
+    # "cogs.special",
+    # "cogs.study",
+    # "cogs.rolemenu"
+    # "cogs.rolereact",
+]
+
+
+@bot.event
+async def on_ready() -> None:
+    print(f"Logged in as {bot.user} at {datetime.fromtimestamp(time.time()).strftime(r'%d-%b-%y, %H:%M:%S')}")
+
+
+async def startup(conf: Config):
+    bot.rolemenu_view_set = False
+    for extension in cogs:
+        try:
+            bot.load_extension(extension)
+            print(f"Successfully loaded extension {extension}")
+        except Exception as e:
+            print(f"Failed to load extension {extension}\n{type(e).__name__,}: {e}")
+
+    await bot.wait_until_ready()
+
+    bot.guild: Guild = await bot.getch_guild(conf.get("guild_id"))
+    bot.db.bot_user_id = bot.user.id
+
+    await bot.resync_slash_commands()
+    print("All Ready")
+
+
+default_colors = {
+    "yellow": 0xFFFF00,
+    "orange": 0xFFA500,
+    "light_orange": 0xFFA07A,
+    "dark_orange": 0xFF5733,
+    "red": 0xFF0000,
+    "green": 0x00FF00,
+    "blue": 0x00FFFF,
+}
+
+default_colors.update({i: int(j, 16) for i, j in conf.get("colors", {}).items()})
+bot.colors = default_colors
+
+
+bot.config = conf
+bot.db = Database(conf)
+
+bot.owner_ids = conf.get("owner_ids", [])
+bot.loop.create_task(startup(bot.config))
+bot.run(bot.config.get("bot_token"))
