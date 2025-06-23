043e1bba5e70827111d63ee3271eb9215ff08d02
Gian Pena <flaconumero2@gmail.com>
2025-06-23 02:18:52 -0500

    Add boostrolemanager cog to bot

---
+++ src/cogs/boostrolemanager.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+
+class BoostTracker(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_member_update(self, before, after):
+        if before.premium_since == after.premium_since or after.premium_since:
+            return
+
+        roles_to_remove = [
+            "nitro-a",
+            "nitro-b",
+            "nitro-c",
+            "nitro-d",
+            "nitro-e",
+            "nitro-f"
+        ]
+
+        for role_name in roles_to_remove:
+            role = discord.utils.get(after.guild.roles, name=role_name)
+            if role not in after.roles: continue
+            await after.remove_roles(role)
