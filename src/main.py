35dab390dec60ea242b6c452fca400b562b358fc
Gian Peña <flaconumero2@gmail.com>
2025-07-20 22:02:39 +0000

    Add boostrolemanager cog to list of cogs

--- src/main.py
+++ src/main.py
@@ -29,20 +29,21 @@ bot: APBot = APBot(
 )
 
 cogs: List[str] = [
-    "cogs.moderation.commands",
-    "cogs.moderation.infraction",
-    "cogs.bonk",
-    "cogs.recurrent",
-    "cogs.tags",
-    "cogs.study",
-    "cogs.events",
-    "cogs.modmail",
-    "cogs.special",
-    "cogs.moderation.appeal",
-    "cogs.moderation.errorhandler",
-    "cogs.moderation.decay",
-    "cogs.rolereact",
-    "cogs.logs",
+     "cogs.moderation.commands",  
+     "cogs.moderation.infraction",
+     "cogs.bonk",  
+     "cogs.recurrent", 
+     "cogs.tags",
+     "cogs.study",
+     "cogs.events",
+     "cogs.modmail",
+     "cogs.special",  
+     "cogs.moderation.appeal",
+     "cogs.moderation.errorhandler",
+     "cogs.moderation.decay",
+     "cogs.rolereact",
+     "cogs.logs",
+     "cogs.boostrolemanager",
 ]
 
 @bot.event
