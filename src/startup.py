f9437d9bb995c2dfc93bb4469c6c7e184735090a
Omer. <68246066+Gokboru-Omer@users.noreply.github.com>
2026-05-25 20:09:11 -0400

    Exam automation is not needed.

--- src/startup.py
+++ src/startup.py
@@ -21,7 +21,7 @@ DEFAULT_COGS = [
     "cogs.boostrolemanager",
     "cogs.role_log",
     "cogs.voice_log",
-    "cogs.exams_automation.day_zero_complete",
+   # "cogs.exams_automation.day_zero_complete",
     "cogs.delete_log",
     "cogs.leak_log",
 ]
